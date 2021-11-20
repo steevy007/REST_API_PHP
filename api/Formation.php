55ea1521f8f3eb80e1220f732e52493ad586611f
Steeve Sanon <steeve.sanon@atalou.info>
2021-11-20 10:37:34 -0500

    rewrite url

--- api/Formation.php
+++ api/Formation.php
@@ -6,23 +6,29 @@ header('Access-Control-Allow-Headers:Access-Control-Allow-Headers,Content-Type,A
 require_once('../dao/formation.dao.php');
 
 if($_SERVER['REQUEST_METHOD']==='GET'){
-    $daoFormation=new DaoFormation();
-    $stmt=$daoFormation->getFormationById($_GET['id']);
-    $formation_item=null;
 
-    while($data=$stmt->fetch(PDO::FETCH_OBJ)){
-        $formation_item=array(
-            'id'=>$data->id,
-            'libelle'=>$data->libelle,
-            'description'=>$data->description,
-            'image'=>$data->image,
-            'categorie_id'=>$data->categorie_id
-        );
-    }
-
-    if($stmt->rowCount()==1){
-        echo json_encode($formation_item);
+    if(isset($_GET['id'])){
+        $daoFormation=new DaoFormation();
+        $stmt=$daoFormation->getFormationById($_GET['id']);
+        $formation_item=null;
+    
+        while($data=$stmt->fetch(PDO::FETCH_OBJ)){
+            $formation_item=array(
+                'id'=>$data->id,
+                'libelle'=>$data->libelle,
+                'description'=>$data->description,
+                'image'=>$data->image,
+                'categorie_id'=>$data->categorie_id
+            );
+        }
+    
+        if($stmt->rowCount()==1){
+            echo json_encode($formation_item);
+        }else{
+            echo json_encode("No Data Found");
+        }
     }else{
-        echo json_encode("No Data Found");
+        echo json_encode("please specifie the id in GET url");
     }
+    
 }
